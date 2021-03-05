4162c0a08454e042a24ed94f751de5af01797252
school <school@LOCAL.local>
2021-03-04 20:09:38 -0500

    update

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignup;
+    ParseUser user = new ParseUser();
 
 
     @Override
@@ -33,6 +37,9 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnLogin.setBackgroundColor(Color.BLUE);
+        btnSignup.setBackgroundColor(Color.GREEN);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,7 +48,18 @@ public class LoginActivity extends AppCompatActivity {
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
             }
-        });
+        }
+        );
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick signup button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                signupUser(username, password);
+                                        }
+                                    }
+        );
 
     }
 
@@ -61,11 +79,32 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+    private void signupUser(String username, String password) {
+        Log.i(TAG, "Atempting to Signup user" + username);
+        user.setUsername(username);
+        user.setPassword(password);
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.e(TAG, "signup succeed",e);
+                } else {
+                    Log.e(TAG, "Issue with Signup",e);
+                    return;
+                }
+
+                Toast.makeText(LoginActivity.this, "Succes!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
 
     private void gotMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         //user are not logged out when clicking on back button
-        finish();
+        //finish();
     }
 }
